7592f5f8487e63aac670cb2dca5207ebdbfe9bd9
Loduis Madariaga <loduis@gmail.com>
2014-12-02 07:39:34 -0500

    Squashed 'src/Illuminate/Filesystem/' changes from daa3549..0af32af
    0af32af Clean up a few things.
    7ccc232 [5.0] Allow S3 region in config/filesystems.php
    8b1abb9 Pass array instead of just integer.
    d7e168e Stricter requirements for <1.0 packages.
    58f32fa Pinning latest Flysystem changes.
    c38d613 general code cleanup
    1952186 Merge pull request #6298 from lucasmichot/5.0-composer-description
    7404aef Replace bindShared() with singleton().
    465de46 Add required description to all composer.json files
    7e1a57a Standardize return null;
    
    git-subtree-dir: src/Illuminate/Filesystem
    git-subtree-split: 0af32af114acb0d048d7de870726339bd350a45b

--- FilesystemManager.php
+++ FilesystemManager.php
@@ -93,12 +93,15 @@ class FilesystemManager implements FactoryContract {
 	 */
 	public function createS3Driver(array $config)
 	{
-		$client = S3Client::factory([
-			'key' => $config['key'], 'secret' => $config['secret'],
-		]);
+		$s3Config = ['key' => $config['key'], 'secret' => $config['secret']];
+
+		if (isset($config['region']))
+		{
+			$s3Config['region'] = $config['region'];
+		}
 
 		return $this->adapt(
-			new Flysystem(new S3Adapter($client, $config['bucket']))
+			new Flysystem(new S3Adapter(S3Client::factory($s3Config), $config['bucket']))
 		);
 	}
 
