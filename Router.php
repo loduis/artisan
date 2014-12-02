12c628ee9747d23a6a9f97ca4a535014c9ac1e17
Loduis Madariaga <loduis@gmail.com>
2014-12-02 07:39:49 -0500

    Squashed 'src/Illuminate/Routing/' changes from ab03df7..8663691
    8663691 Cleaning up merge conflicts.
    8d241a6 Fixed bug where array based group filters would not be merged with filters applied to a route using before/after method.
    f1805de Add the request namespace to stubbed controllers.
    bcff0c0 Fixes few illuminate/routing docblock.
    5cdf008 general code cleanup
    52a5350 Allow dynamic access of route parameters.
    200bd7e Merge pull request #6334 from lucasmichot/5.0-useless-else
    48775ea Fix docblock
    35ac89f Remove useless else
    f252115 Merge pull request #6269 from lucasmichot/5.0-try-catch
    a6a5c13 Merge pull request #6298 from lucasmichot/5.0-composer-description
    f38a2cd Replace bindShared() with singleton().
    b04f52c Extend base controller by default.
    6d4c617 Add required description to all composer.json files
    2de11c2 Add controller inspector.
    104fc15 Fix try/catch formatting
    aeb6427 Merge pull request #6227 from lucasmichot/5.0-return-null
    8e8526a Merge pull request #6224 from GrahamCampbell/5.0-cs
    124439d Merge pull request #6229 from lucasmichot/5.0-endpoint-trait
    9b1a317 Removed unused use
    10f96dc Add FQCN
    df31ce1 Add EndpointTrait
    af1341b Standardize return null;
    cffcbee CS fixes
    
    git-subtree-dir: src/Illuminate/Routing
    git-subtree-split: 8663691007cbe1d1f14ad6e259efd08ea8284e94

--- Router.php
+++ Router.php
@@ -6,8 +6,6 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Contracts\Routing\RoutableInterface;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Illuminate\Contracts\Routing\Registrar as RegistrarContract;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -212,9 +210,94 @@ class Router implements RegistrarContract {
 	}
 
 	/**
+	 * Register an array of controllers with wildcard routing.
+	 *
+	 * @param  array  $controllers
+	 * @return void
+	 */
+	public function controllers(array $controllers)
+	{
+		foreach ($controllers as $uri => $name)
+		{
+			$this->controller($uri, $name);
+		}
+	}
+
+	/**
+	 * Route a controller to a URI with wildcard routing.
+	 *
+	 * @param  string  $uri
+	 * @param  string  $controller
+	 * @param  array   $names
+	 * @return void
+	 */
+	public function controller($uri, $controller, $names = array())
+	{
+		$prepended = $controller;
+
+		// First, we will check to see if a controller prefix has been registered in
+		// the route group. If it has, we will need to prefix it before trying to
+		// reflect into the class instance and pull out the method for routing.
+		if ( ! empty($this->groupStack))
+		{
+			$prepended = $this->prependGroupUses($controller);
+		}
+
+		$routable = (new ControllerInspector)
+		                    ->getRoutable($prepended, $uri);
+
+		// When a controller is routed using this method, we use Reflection to parse
+		// out all of the routable methods for the controller, then register each
+		// route explicitly for the developers, so reverse routing is possible.
+		foreach ($routable as $method => $routes)
+		{
+			foreach ($routes as $route)
+			{
+				$this->registerInspected($route, $controller, $method, $names);
+			}
+		}
+
+		$this->addFallthroughRoute($controller, $uri);
+	}
+
+	/**
+	 * Register an inspected controller route.
+	 *
+	 * @param  array   $route
+	 * @param  string  $controller
+	 * @param  string  $method
+	 * @param  array   $names
+	 * @return void
+	 */
+	protected function registerInspected($route, $controller, $method, &$names)
+	{
+		$action = array('uses' => $controller.'@'.$method);
+
+		// If a given controller method has been named, we will assign the name to the
+		// controller action array, which provides for a short-cut to method naming
+		// so you don't have to define an individual route for these controllers.
+		$action['as'] = array_get($names, $method);
+
+		$this->{$route['verb']}($route['uri'], $action);
+	}
+
+	/**
+	 * Add a fallthrough route for a controller.
+	 *
+	 * @param  string  $controller
+	 * @param  string  $uri
+	 * @return void
+	 */
+	protected function addFallthroughRoute($controller, $uri)
+	{
+		$missing = $this->any($uri.'/{_missing}', $controller.'@missingMethod');
+
+		$missing->where('_missing', '(.*)');
+	}
+
+	/**
 	 * Route a resource to a controller.
 	 *
-	 * @param  Router  $router
 	 * @param  string  $name
 	 * @param  string  $controller
 	 * @param  array   $options
@@ -792,7 +875,7 @@ class Router implements RegistrarContract {
 	{
 		$this->bind($key, function($value) use ($class, $callback)
 		{
-			if (is_null($value)) return null;
+			if (is_null($value)) return;
 
 			// For model binders, we will attempt to retrieve the models using the first
 			// method on the model instance. If we cannot retrieve the models we'll
