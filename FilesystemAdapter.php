32f053ab7323eeefa0c61596efe0cf07290abb76
Loduis Madariaga <loduis@gmail.com>
2016-11-19 11:38:38 -0500

    Squashed 'src/Illuminate/Filesystem/' changes from 10256fd..d231219
    d231219 Merge branch '5.3'
    82576e0 tweak config name
    7536552 StyleCi formatting
    ed3e70d Use 'url_root' config value to generate disk url
    1f6af80 fix
    4e51db2 close #15142 make the FilesystemAdapter::delete() behave like FileSystem::delete() (#15308)
    08664d1 Merge remote-tracking branch 'origin/5.3' into 5.3
    8fdc1e1 fix method order
    dcbe7b3 Change url generating logic (#15291)
    f36a6e9 This closes #14832 by adding isReadable
    
    git-subtree-dir: src/Illuminate/Filesystem
    git-subtree-split: d23121947acf175a365afedf8f40af44ab994888

--- FilesystemAdapter.php
+++ FilesystemAdapter.php
@@ -198,15 +198,19 @@ class FilesystemAdapter implements FilesystemContract, CloudFilesystemContract
     {
         $paths = is_array($paths) ? $paths : func_get_args();
 
+        $success = true;
+
         foreach ($paths as $path) {
             try {
-                $this->driver->delete($path);
+                if (! $this->driver->delete($path)) {
+                    $success = false;
+                }
             } catch (FileNotFoundException $e) {
-                //
+                $success = false;
             }
         }
 
-        return true;
+        return $success;
     }
 
     /**
@@ -276,16 +280,23 @@ class FilesystemAdapter implements FilesystemContract, CloudFilesystemContract
     {
         $adapter = $this->driver->getAdapter();
 
-        if ($adapter instanceof AwsS3Adapter) {
+        if (method_exists($adapter, 'getUrl')) {
+            return $adapter->getUrl($path);
+        } elseif ($adapter instanceof AwsS3Adapter) {
             $path = $adapter->getPathPrefix().$path;
 
             return $adapter->getClient()->getObjectUrl($adapter->getBucket(), $path);
         } elseif ($adapter instanceof LocalAdapter) {
+            $config = $this->driver->getConfig();
+
+            if ($config->has('url')) {
+                return $config->get('url').'/'.$path;
+            }
+
             $path = '/storage/'.$path;
 
-            return Str::contains($path, '/storage/public') ? Str::replaceFirst('/public', '', $path) : $path;
-        } elseif (method_exists($adapter, 'getUrl')) {
-            return $adapter->getUrl($path);
+            return Str::contains($path, '/storage/public') ?
+                        Str::replaceFirst('/public', '', $path) : $path;
         } else {
             throw new RuntimeException('This driver does not support retrieving URLs.');
         }
