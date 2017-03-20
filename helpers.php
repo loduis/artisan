3610bfe9ced6504493bf2a4254f910582da1593b
Loduis Madariaga <loduis@gmail.com>
2017-03-20 10:00:12 -0500

    Squashed 'src/Illuminate/Foundation/' changes from dc87e14..b7c43ef
    b7c43ef fix broken shortcut
    bdd9d0e Merge branch 'pretty-print' of https://github.com/browner12/framework into browner12-pretty-print
    98dd562 add ability to pretty print response
    e8e7419 add files
    0e9bf2a working on formatting
    7f11569 Merge branch 'soft-delete-assertion' of https://github.com/browner12/framework into browner12-soft-delete-assertion
    ab5ab03 Listener make command short queued option (#18362)
    18191bf style fix
    077e486 add assertion for soft deleted models
    e80714b [5.4] Brings back to app() the ability to make instances passing parameters (#18320)
    726851e [5.4] AliasLoader docblock and code (#18310)
    0000040         mind file uploads in nested array keys (#18276)
    39b4a3b Fixed phpdoc
    e828172 change exception type
    8cdfaa7 Merge branch 'bootstrap_cache_exception' of https://github.com/macdabby/framework into macdabby-bootstrap_cache_exception
    65ad76f Detect when running in PHPDBG (#18198)
    b231550 version
    90d7f1e Proxy isset() checks on TestResponse (#18182)
    dea6e08 version
    2fbd367 formatting
    5af108f Added exception error when bootstrap/cache directory is not writable
    8ab7391 Add context to the assert json test response method
    507f1f0 [5.4] Use composition to create TestResponses instead of inheritance (#18089)
    7e431e7 while doing `npm run hot`, do not require existence of the manifest file (#18088)
    bb19b98 version
    44d8235 Fix wentDownAt having wrong time if using retryAfter (#17991)
    cf11efd Simplify the command to generate resource controllers from make:model (#17955)
    a58c17f version
    10381e3 add asssertion for session errors
    5d46b2e simplify implementation
    dce2600 check for files
    a1fcf9f version
    5b3fcef Fixed (#17835)
    fca2c50 increment version
    4217782 move method
    1c49c9a formatting
    5ed65ee Add assertJsonFragment()
    5fffec6 Fix last old-style queue job fired by the core.
    8be9a81 Ensure that the method "map" exists before calling (#17784)
    011f9eb add second param
    31408c7 remove second param from json structure
    f88390e Merge branch '5.3' into 5.4
    cd9497b rename variable
    a091d16 Merge branch '5.4' of https://github.com/SeoFood/framework into SeoFood-5.4
    d5ee0c0 fixes cs
    b4075bc fixed cs
    cdf9795 fixed cs
    18a8463 version
    92d2278 Properly set the JSON payload of FormRequests instead of re-building it
    21efe37 allow multiple manifest files for mix helper
    6b8892a TrimString bugfixes (using strict comparison instead of loose comparison) (#17741)
    35c91d9 update doc block
    d69ee9d Add json alias for decodeResponseJson method
    5ea3fdb Don't wrap asset path in default domain (#17727)
    70ebb50 Update MaintenanceModeException.php (#17720)
    de5cad9 [5.4] Use Macroable on TestResponse (#17726)
    3c3b99b version
    
    git-subtree-dir: src/Illuminate/Foundation
    git-subtree-split: b7c43ef31b21c72ed1a8c21d509669409d9fa2f6

--- helpers.php
+++ helpers.php
@@ -94,15 +94,18 @@ if (! function_exists('app')) {
      * Get the available container instance.
      *
      * @param  string  $abstract
+     * @param  array   $parameters
      * @return mixed|\Illuminate\Foundation\Application
      */
-    function app($abstract = null)
+    function app($abstract = null, array $parameters = [])
     {
         if (is_null($abstract)) {
             return Container::getInstance();
         }
 
-        return Container::getInstance()->make($abstract);
+        return empty($parameters)
+            ? Container::getInstance()->make($abstract)
+            : Container::getInstance()->makeWith($abstract, $parameters);
     }
 }
 
@@ -553,25 +556,33 @@ if (! function_exists('mix')) {
      * Get the path to a versioned Mix file.
      *
      * @param  string  $path
+     * @param  string  $manifestDirectory
      * @return \Illuminate\Support\HtmlString
      *
      * @throws \Exception
      */
-    function mix($path)
+    function mix($path, $manifestDirectory = '')
     {
         static $manifest;
-        static $shouldHotReload;
+
+        if (! starts_with($path, '/')) {
+            $path = "/{$path}";
+        }
+
+        if ($manifestDirectory && ! starts_with($manifestDirectory, '/')) {
+            $manifestDirectory = "/{$manifestDirectory}";
+        }
+
+        if (file_exists(public_path($manifestDirectory.'/hot'))) {
+            return new HtmlString("http://localhost:8080{$path}");
+        }
 
         if (! $manifest) {
-            if (! file_exists($manifestPath = public_path('mix-manifest.json'))) {
+            if (! file_exists($manifestPath = public_path($manifestDirectory.'/mix-manifest.json'))) {
                 throw new Exception('The Mix manifest does not exist.');
             }
 
             $manifest = json_decode(file_get_contents($manifestPath), true);
-        }
-
-        if (! starts_with($path, '/')) {
-            $path = "/{$path}";
         }
 
         if (! array_key_exists($path, $manifest)) {
@@ -581,9 +592,7 @@ if (! function_exists('mix')) {
             );
         }
 
-        return $shouldHotReload = file_exists(public_path('hot'))
-                    ? new HtmlString("http://localhost:8080{$manifest[$path]}")
-                    : new HtmlString(url($manifest[$path]));
+        return new HtmlString($manifestDirectory.$manifest[$path]);
     }
 }
 
@@ -667,7 +676,7 @@ if (! function_exists('request')) {
             return app('request')->only($key);
         }
 
-        return app('request')->input($key, $default);
+        return data_get(app('request')->all(), $key, $default);
     }
 }
 
